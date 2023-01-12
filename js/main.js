6f08cad322ce70b67815daf12a7159a0e55b2085
Cesar <Csaguier78@gmail.comm>
2023-01-12 19:45:15 -0300

    Primer repositorio

---
+++ js/main.js
@@ -0,0 +1,547 @@
+/* -- Strict mode enabled -- */
+'use strict';
+
+
+// Initialize web3
+let web3;
+
+// Connect to MetaMask
+function connectMetaMask() {
+    // Check if MetaMask is installed
+    // code to connect to MetaMask goes here
+      
+    if (typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
+        var web3 = new Web3(window.ethereum);
+        
+        window.ethereum.enable().then(function (accounts) {
+            document.getElementById("connectButton").style.display = 'none';
+            console.log("Connect button clicked");
+            
+        });
+    } else if (typeof window.web3 !== 'undefined' && window.web3.currentProvider.isMetaMask) {
+        var web3 = new Web3(window.web3.currentProvider);
+        document.getElementById("connectButton").style.display = 'none';
+        console.log("Connect button clicked");
+    } else {
+        alert('Please install MetaMask');
+    }
+    
+}
+
+
+// Add an event listener for when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Check if web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        // Use the web3 provider that has been injected
+        web3 = new Web3(web3.currentProvider);
+        if(web3.currentProvider.isMetaMask){
+            console.log("MetaMask is already connected");
+        }
+        else{
+            var button = document.getElementById("connectButton");
+            button.style.display = 'block';
+            button.addEventListener('click', function(){
+                web3.currentProvider.enable().then(() => {
+                    console.log('MetaMask is now connected')
+                });
+            });
+        }
+    } else {
+        console.log('MetaMask is not installed')
+    }
+});
+
+  
+const initialize = () => {
+    //Basic Actions Section
+    const onboardButton = document.getElementById('connectButton');
+  
+    //Created check function to see if the MetaMask extension is installed
+    const isMetaMaskInstalled = () => {
+      //Have to check the ethereum binding on the window object to see if it's installed
+      const { ethereum } = window;
+      return Boolean(ethereum && ethereum.isMetaMask);
+    };
+  
+    //------Inserted Code------\\
+    const MetaMaskClientCheck = () => {
+        //Now we check to see if Metmask is installed
+        if (!isMetaMaskInstalled()) {
+          //If it isn't installed we ask the user to click to install it
+          onboardButton.innerText = 'Click here to install MetaMask!';
+          //When the button is clicked we call this function
+          onboardButton.onclick = onClickInstall;
+          //The button is now disabled
+          onboardButton.disabled = true;
+        } else {
+          //If it is installed we change our button text
+          onboardButton.innerText = 'Connect';
+        }
+      };
+      MetaMaskClientCheck();
+}
+
+
+const onClickConnect = async () => {
+    try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      await ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+$(function () {
+
+    var _window = $(window),
+        _document = $(document),
+        _body = $('body');
+
+    //animated navbar-toggler button
+    _document.on('click', '.navbar .navbar-toggler', function () {
+        $(this).toggleClass("change");
+    });
+
+    _document.on('click', '.o-language .dropdown-toggle', function (e) {
+        if (_window.width() < 768) {
+            e.preventDefault();
+            $(this).next('.dropdown-menu').slideToggle();
+        }
+    });
+
+    //Background image rotation on scroll
+    function AnimateRotate(d) {
+        var elem = $(".banner-poly-bg");
+
+        $({
+            deg: 0
+        }).animate({
+            deg: d
+        }, {
+            duration: 2000,
+            step: function (now) {
+                elem.css({
+                    transform: "rotate(" + now + "deg)"
+                });
+            }
+        });
+    }
+
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+        target: ".navbar-nav",
+        offset: 100
+    });
+
+    //script for page scroll to top and bottom
+    _document.on('click', '.navbar-nav li a:not(.dropdown-toggle),.footer-widget li a, .page-scroll', function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 80
+                }, 800, 'easeInOutExpo');
+                return false;
+            }
+        }
+    });
+
+    //Wow js
+    var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 200, // distance to the element when triggering the animation (default is 0)
+        mobile: true, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function (box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    });
+    wow.init();
+
+    //jQuery countdown plugin
+    $('#clock').countdown('2023/01/20').on('update.countdown', function (event) {
+        var _DateInput = '' +
+            '<div><span>%-D</span> Day%!d</div>' +
+            '<div><span>%H</span> Hours</div>' +
+            '<div><span>%M</span> Minutes</div>' +
+            '<div><span>%S</span> Seconds</div>';
+        var $this = $(this).html(event.strftime(_DateInput));
+    });
+
+    //progress bar
+    $(".progress").each(function () {
+        $(this).waypoint(function () {
+            $('.progress-bar').progressbar({
+                transition_delay: 100
+            });
+        }, {
+            triggerOnce: true,
+            offset: 'bottom-in-view'
+        });
+    });
+
+    // Clients slider
+    $('.c-onepage-clients-slider').slick({
+        // dots: true,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    //script for popup video modal 
+    $('.video-popup').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close">fssdfsdf</div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: '//www.youtube.com/embed/%id%?autoplay=1'
+                }
+            },
+            srcAction: 'iframe_src'
+        },
+        closeMarkup: '<button title="%title%" type="button" class="mfp-close">x</button>',
+        mainClass: 'mfp-fade',
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            opener: function (openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
+
+    $('.o-roadmap').each(function () {
+        $(this).on('click', ".o-common-card", function (e) {
+            e.preventDefault();
+            $(this).find('.collapse-txt').slideToggle("3000");
+            $(this).find('.move-down').toggleClass("rotate");
+        });
+    });
+
+    //pie chart
+    AmCharts.makeChart("chartdiv", {
+        "type": "pie",
+        "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+        "innerRadius": "40%",
+        "autoMargins": false,
+        "addClassNames": true,
+        "svgIcons": false,
+        "colors": [
+            "#448aff",
+            "#543bcc",
+            "#07d79c",
+            "#f5354e",
+            "#ff9b53",
+            "#B0DE09",
+            "#04D215",
+            "#0D8ECF",
+            "#0D52D1",
+            "#2A0CD0",
+            "#8A0CCF",
+            "#CD0D74",
+            "#754DEB",
+            "#DDDDDD",
+            "#999999",
+            "#333333",
+            "#000000",
+            "#57032A",
+            "#CA9726",
+            "#990000",
+            "#4B0C25"
+        ],
+        "labelsEnabled": false,
+        "titleField": "category",
+        "valueField": "column-1",
+        "startDuration": 0,
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [{
+                "category": "Airdrop",
+                "column-1": "1"
+            },
+            {
+                "category": "Development",
+                "column-1": "20"
+            },
+            {
+                "category": "Liquidity farming",
+                "column-1": "26"
+            },
+            {
+                "category": "Marketing & Advisors",
+                "column-1": "8"
+            },
+            {
+                "category": "Team founder",
+                "column-1": "20"
+            },
+            {
+                "category": "Seed, Private sale A & B",
+                "column-1": "25"
+            }
+        ]
+    });
+
+    // Chart
+    (function (b, i, t, C, O, I, N) {
+        window.addEventListener('load', function () {
+            if (b.getElementById(C)) return;
+            I = b.createElement(i), N = b.getElementsByTagName(i)[0];
+            I.src = t;
+            I.id = C;
+            N.parentNode.insertBefore(I, N);
+        }, false)
+    });
+
+    //currency converter
+    _document.on('click', '.o-currencies span, .o-coins span', function () {
+        $(this).prevAll().removeClass('active');
+        $(this).nextAll().removeClass('active');
+        $(this).addClass('active');
+        _convertCurrency();
+
+    });
+
+    function _convertCurrency() {
+        var _oConverter = $('.o-converter-tab .tab-pane.active'),
+            _inputAmount = _oConverter.find('.input-amount').val(),
+            _pattern = /^[0-9]*$/,
+            _usd = "$",
+            _eur = "€",
+            _isValid = _pattern.test(_oConverter.find('.input-amount').val()),
+            _getInputAmount = parseInt(_oConverter.find('.input-amount').val());
+
+        if (_oConverter.find('.usd').hasClass('active')) {
+            _oConverter.find('.currency-sign').text(_usd);
+            var _cValue = parseInt(_oConverter.find('.o-coins').find('span.active').attr('data-value-usd'));
+        } else {
+            _oConverter.find('.currency-sign').text(_eur);
+            var _cValue = parseInt(_oConverter.find('.o-coins').find('span.active').attr('data-value'));
+        }
+        var _getTotal = _getInputAmount * _cValue;
+
+        if (!_isValid || _oConverter.find('.input-amount').val() === "" || _oConverter.find('.input-amount').val() == undefined) {
+            _oConverter.find('.value-err').fadeIn('500');
+            $('.input-amount').val("");
+        } else {
+            _oConverter.find('.currency-total').text(_getTotal);
+            _oConverter.find('.value-err').fadeOut('500');
+        }
+    }
+
+    $('.o-converter').on('click', '.btn', function () {
+        _convertCurrency();
+    });
+
+    $('.o-converter').on('focus keyup blur', 'input', function () {
+        $(this).next('.value-err').fadeOut('500');
+    });
+
+
+    //flip map
+    var _txt = '<i class="icon-Store"></i>VIEW MAP',
+        _alterTxt = '<i class="icon-mail"></i>CONTACT US';
+    $.fn.extend({
+        toggleText: function (a, b) {
+            return this.html(this.html() == b ? a : b);
+        }
+    });
+
+    $('.o-contact-info').on('click', '.btn', function (e) {
+        e.preventDefault();
+        $('.flip-container').toggleClass('flip-me');
+        $(".o-contact-info .btn").toggleText(_txt, _alterTxt);
+
+        if (_window.width() < 768) {
+            $('html, body').animate({
+                scrollTop: $(".flip-container").offset().top - 100
+            }, 1000);
+        }
+    });
+
+    //Global Form validation
+    $('.o-contact-form').on('submit', function (e) {
+        e.preventDefault();
+        var _self = $(this),
+            data = $(this).serialize(),
+            __selector = _self.closest('input,textarea');
+
+        _self.closest('div').find('input,textarea').removeAttr('style');
+        _self.find('.err-msg').remove();
+        _self.find('.form-success').removeClass('form-success');
+
+        $('.submit-loading-img').css('display', 'block');
+        _self.closest('div').find('button[type="submit"]').attr('disabled', 'disabled');
+
+        $.ajax({
+            url: 'email/email.php',
+            type: "post",
+            dataType: 'json',
+            data: data,
+            success: function (data) {
+                $('.submit-loading-img').css('display', 'none');
+                _self.closest('div').find('button[type="submit"]').removeAttr('disabled');
+                if (data.code == false) {
+                    _self.closest('div').find('[name="' + data.field + '"]').addClass('form-success');
+                    _self.closest('div').find('[name="' + data.field + '"]').after('<div class="err-msg">*' + data.err + '</div>');
+                } else {
+                    _self.find('textarea:last-child').after('<div class="success-msg">' + data.success + '</div>');
+                    _self[0].reset();
+                    setTimeout(function () {
+                        $('.success-msg').fadeOut('slow');
+                    }, 5000);
+                }
+            }
+        });
+    });
+
+    // Preloader js
+    function loader(_success) {
+        var obj = $('.o-preloader'),
+            inner = $('.o-preloader_inner');
+        var w = 0,
+            t = setInterval(function () {
+                w = w + 1;
+                inner.text(w + '%');
+                if (w === 100) {
+                    obj.addClass('open-page');
+                    obj.addClass('hide-loader');
+
+                    clearInterval(t);
+                    w = 0;
+                    if (_success) {
+                        return _success();
+                    }
+                }
+            }, 20);
+    }
+
+    //Close menu when clicked menu-items or outside
+    $(".c-onepage-navbar").on('click', 'ul li a:not(.dropdown-toggle)', function () {
+        $('.navbar-collapse').removeClass('show');
+        $('.navbar-toggler').removeClass('change');
+    });
+
+    _document.on('click', function (e) {
+        var _navMenu = $('.navbar-nav li');
+        if ($('.navbar-collapse').hasClass('show')) {
+            if (!_navMenu.is(e.target) && _navMenu.has(e.target).length === 0) {
+                $('.navbar-collapse').removeClass('show');
+                $(".navbar-toggler").removeClass('change');
+            }
+        }
+    });
+
+    // show hide subnav depending on scroll direction
+    var _position = 0;
+
+    _window.on('scroll', function () {
+        var _scroll = $(this).scrollTop();
+
+        if (_position > _scroll) {
+            $('.banner-poly-bg-1').css({
+                "bottom": "-=5px"
+            });
+            $('.banner-poly-bg-2').css({
+                "bottom": "-=5px"
+            });
+            $('.banner-poly-bg-3').css({
+                "bottom": "-=5px"
+            });
+        } else if (_scroll > _position) {
+            $('.banner-poly-bg-1').css({
+                "bottom": "+=5px"
+            });
+            $('.banner-poly-bg-2').css({
+                "bottom": "+=5px"
+            });
+            $('.banner-poly-bg-3').css({
+                "bottom": "+=5px"
+            });
+        }
+
+        _position = _scroll;
+    });
+
+    //Preloader
+    loader();
+
+}());
+
+
+//Google map
+function initMap() {
+    var _location = {
+        lat: 40.712811,
+        lng: -73.997745
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: _location
+    });
+    var marker = new google.maps.Marker({
+        position: _location,
+        map: map
+        // icon: "images/marker.png"
+    });
+}
+
+window.connectMetaMask = connectMetaMask;
